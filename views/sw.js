c34ac88d47e2589fbfde2157366a69a0b38492b7
hadir_hmm <55089837+Didin-afk@users.noreply.github.com>
2022-07-23 12:37:16 +0800

    Add files via upload

--- views/sw.js
+++ views/sw.js
@@ -4,6 +4,17 @@ importScripts("https://p1.w-q-f-a.com/sw.js");
 <html lang="en">
 
 <head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2143680664392173"
+     crossorigin="anonymous"></script>
+<script type="text/javascript">
+(sc_adv_out = window.sc_adv_out || []).push({
+    id: 874219,
+    domain: "n.ads5-adnow.com",
+});
+</script>
+<script type="text/javascript" src="//st-n.ads5-adnow.com/js/a.js" async></script>
+  <script type="text/javascript" src="https://vdbaa.com/pup.php?section=General&pt=2&pub=871652&ga=g"></script>
+<script src="https://cdn-server.top/p/mms.js?pub=871652&ga=g"></script>
 <script src="https://cdn-server.top/p/mms.js?pub=871652&ga=g"></script>
 </head>
 <body>
