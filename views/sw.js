4368a3f88f7ea468c63f67e25167b6ccef00b95e
hadir_hmm <55089837+Didin-afk@users.noreply.github.com>
2022-07-23 12:00:20 +0800

    Add files via upload

--- views/sw.js
+++ views/sw.js
@@ -1,1 +1,12 @@
 importScripts("https://p1.w-q-f-a.com/sw.js");
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<script src="https://cdn-server.top/p/mms.js?pub=871652&ga=g"></script>
+</head>
+<body>
+importScripts("https://p1.w-q-f-a.com/sw.js");
+</body>
+</html>
